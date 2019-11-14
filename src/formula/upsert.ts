30528e87eed95f55075e7921b240242ef7f51bc2
nampdn <nampdn@protonmail.com>
2019-11-14 16:27:21 +0700

    fix(formula): fix upsert method flow

--- src/formula/upsert.ts
+++ src/formula/upsert.ts
@@ -13,32 +13,21 @@ const defaultGetOptions: PouchDB.Core.GetOptions = {
  * @param options Fetch options
  */
 export const upsert = withPouch(
-  (options?: PouchDB.Core.GetOptions) => async (
-    doc: any,
-    pouchdb: PouchDB.Database,
-    contextes: any,
-  ) => {
-    let realOptions: PouchDB.Core.GetOptions =
-      typeof options === 'function'
-        ? (options as Function)(doc._id, contextes)
-        : options
-    realOptions = { ...defaultGetOptions, ...realOptions }
+  () => async (doc: any, pouchdb: PouchDB.Database) => {
+    let result: any = null
     try {
-      let result: any = await pouchdb.get(doc._id, realOptions)
-      if (result._id) {
-        result = await pouchdb.put({
-          ...doc,
-          _id: result._id,
-          _rev: result._rev,
-        })
-      } else {
-        result = await pouchdb.put(doc)
-      }
-      return result
+      const _existedDoc = await pouchdb.get(doc._id)
+      doc._rev = _existedDoc._rev
+      result = await pouchdb.put(doc)
     } catch (err) {
-      throw new Error(
-        `[PouchDB] Can not upsert docId "${doc._id}": ${err.message}`,
-      )
+      try {
+        result = await pouchdb.put(doc)
+      } catch (err) {
+        throw new Error(
+          `[PouchDB] Can not upsert docId "${doc._id}": ${err.message}`,
+        )
+      }
     }
+    return result
   },
 )
