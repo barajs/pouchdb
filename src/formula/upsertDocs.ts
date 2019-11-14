e3f07b32ec84917fa997329dee731bb84c095c32
nampdn <nampdn@protonmail.com>
2019-11-14 16:54:28 +0700

    fix(upsertDocs): rename from bulkDocsUpsert method
    change algorithm

--- src/formula/upsertDocs.ts
+++ src/formula/upsertDocs.ts
@@ -1,22 +1,24 @@
 import { withPouch } from './withPouch'
 
-const prepareDoc = async (doc: any, pouchdb: PouchDB.Database) => {
-  const docRev = await pouchdb.get(doc._id)
-  if (!docRev._rev) {
+const getDocRev = async (doc: any, pouchdb: PouchDB.Database) => {
+  try {
+    const docRev = await pouchdb.get(doc._id)
+    doc._rev = docRev._rev
+    return doc
+  } catch (err) {
     return doc
   }
-  return { ...doc, _rev: docRev._rev }
 }
 
 /**
  * Upsert bulk multiple docs in a single query.
  */
-export const bulkDocsUpsert = withPouch(
+export const upsertDocs = withPouch(
   () => async (docs: any[], pouchdb: PouchDB.Database) => {
     try {
       let docsWithRev = []
       for (const doc of docs) {
-        const docRev = await prepareDoc(doc, pouchdb)
+        const docRev = await getDocRev(doc, pouchdb)
         docsWithRev.push(docRev)
       }
       const result = await pouchdb.bulkDocs(docsWithRev)
